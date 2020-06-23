7c0ff1c684d33dc0773064f588a2a53ef5df641f
phil cowan <philipjcowan1@gmail.com>
2020-06-23 14:48:47 -0700

    navigation and first form setup

---
+++ src/screens/SignupScreen.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { View, StyleSheet, } from 'react-native';
+import { Text, Input, Button } from 'react-native-elements'
+import Spacer from './components/Spacer';
+
+const SignupScreen = ({ navigation }) => {
+    return (
+        <View style={styles.container}>
+            <Spacer />
+            <Text h3 >Sign Up for Tracker</Text>
+            <Spacer />
+            <Input label="Email" />
+            <Spacer />
+            <Input label="Password" />
+            <Spacer>
+                <Button title="Sign Up" />
+            </Spacer>
+        </View>
+    )
+};
+
+SignupScreen.navigationOptions = () => {
+    return {
+        header: () => false,
+    };
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        marginBottom: 200
+    }
+});
+
+export default SignupScreen;
