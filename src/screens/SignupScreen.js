95a689bcbf10b4723e9d9fd54ad01ff959daabcc
phil cowan <philipjcowan1@gmail.com>
2020-06-23 15:29:50 -0700

    adds secure text entry for password and some other styles

--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -1,17 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View, StyleSheet, } from 'react-native';
 import { Text, Input, Button } from 'react-native-elements'
 import Spacer from './components/Spacer';
 
 const SignupScreen = ({ navigation }) => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
     return (
-        <View style={styles.container}>
+        <View style={styles.containerStyle}>
             <Spacer />
             <Text h3 >Sign Up for Tracker</Text>
             <Spacer />
-            <Input label="Email" />
+            <Input
+                label="Email"
+                autoCapitalize="none"
+                autoCorrect={false}
+                value={email}
+                onChangeText={setEmail} />
             <Spacer />
-            <Input label="Password" />
+            <Input
+                secureTextEntry
+                label="Password"
+                autoCapitalize="none"
+                autoCorrect={false}
+                value={password}
+                onChangeText={setPassword} />
             <Spacer>
                 <Button title="Sign Up" />
             </Spacer>
@@ -26,7 +39,7 @@ SignupScreen.navigationOptions = () => {
 }
 
 const styles = StyleSheet.create({
-    container: {
+    containerStyle: {
         flex: 1,
         justifyContent: 'center',
         marginBottom: 200
