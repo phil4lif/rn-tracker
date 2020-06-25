21cf433533924c371079fefa2b6e4ead7eaad4cf
phil cowan <philipjcowan1@gmail.com>
2020-06-25 13:13:23 -0700

    refactor signin screen with new navlink and authform components

--- src/screens/SigninScreen.js
+++ src/screens/SigninScreen.js
@@ -1,10 +1,37 @@
 import React from 'react';
 import { View, StyleSheet, Text } from 'react-native';
+import AuthForm from '../components/AuthForm';
+import NavLink from '../components/NavLink';
 
 const SigninScreen = () => {
-    return <Text style={{ fontSize: 48 }}>SigninScreen</Text>;
+    return (
+        <View style={styles.containerStyle}>
+            <AuthForm
+                headerText="Sign in to Your Account"
+                errorMessage=""
+                onSubmit={() => { }}
+                submitButtonText="Sign In"
+            />
+            <NavLink
+                text="Don't have an account? Sign up instead."
+                routeName="Signup"
+            />
+        </View>
+    )
 };
 
-const styles = StyleSheet.create({});
+SigninScreen.navigationOptions = () => {
+    return {
+        header: () => false,
+    };
+};
+
+const styles = StyleSheet.create({
+    containerStyle: {
+        flex: 1,
+        justifyContent: 'center',
+        marginBottom: 200
+    },
+});
 
 export default SigninScreen;
