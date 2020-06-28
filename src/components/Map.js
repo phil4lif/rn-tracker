fbb5b4eb378ac3704d7d5097d40bdadfc1dcc185
phil cowan <philipjcowan1@gmail.com>
2020-06-27 19:30:28 -0700

    adds MapView to the trackcreatescreen

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,12 +1,24 @@
 import React from 'react';
-import {View, Text, StyleSheet} from 'react-native'
+import {View, Text, StyleSheet} from 'react-native';
+import MapView from 'react-native-maps';
 
 const Map = () => {
     return (
-        <Text>I am a map</Text>
+        <MapView 
+        style={styles.map}
+        initialRegion={{
+            latitude: 37.33233,
+            longitude: -122.03121,
+            latitudeDelta: 0.01,
+            longitudeDelta: 0.01
+        }} />
     )
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    map: {
+        height: 300
+    }
+});
 
 export default Map;
