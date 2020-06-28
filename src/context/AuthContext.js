a25d49520a17c7a7f706fd12ef7cd039f536019b
phil cowan <philipjcowan1@gmail.com>
2020-06-27 17:52:48 -0700

    signout functionality

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -11,6 +11,8 @@ const authReducer = (state, action) => {
             return { errorMessage: '', token: action.payload };
         case 'clear_error_message':
             return { ...state, errorMessage: '' }
+        case 'signout':
+            return { token: null, errorMessage: ''}
         default:
             return state;
     }
@@ -56,11 +58,12 @@ const signin = dispatch => async ({ email, password }) => {
     }
 }
 
-const signout = (dispatch) => {
-    return () => {
-        //signout
-    }
-}
+const signout = dispatch => async () => {
+    await AsyncStorage.removeItem('token');
+    dispatch({ type: 'signout' })
+    navigate('loginFlow');
+    };
+
 export const { Provider, Context } = createDataContext(
     authReducer,
     { signin, signout, signup, clearErrorMessage, tryLocalSignin },
