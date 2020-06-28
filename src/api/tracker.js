7353561679d7db24ab89e815454c66e85628e8a3
phil cowan <philipjcowan1@gmail.com>
2020-06-28 16:09:25 -0700

    changing axios instance to include auth headers

--- src/api/tracker.js
+++ src/api/tracker.js
@@ -1,5 +1,20 @@
 import axios from 'axios';
+import { AsyncStorage } from 'react-native';
 
-export default axios.create({
+const instance = axios.create({
     baseURL: 'http://786e2b2d0795.ngrok.io'
-})
+});
+
+instance.interceptors.request.use(
+    async (config) => {
+        const token = await AsyncStorage.getItem('token');
+        if (token) {
+            config.headers.Authorization = `Bearer ${token}`;
+        }
+        return config;
+    },
+    (err) => {
+        return Promise.reject(err);
+    }
+);
+export default instance;
--- src/components/TrackForm.js
+++ src/components/TrackForm.js
@@ -2,14 +2,16 @@ import React, { useContext } from 'react';
 import { Input, Button } from 'react-native-elements';
 import Spacer from './Spacer';
 import { Context as LocationContext } from '../context/LocationContext';
+import useSaveTrack from '../hooks/useSaveTrack';
 
 const TrackForm = () => {
     const { state: { name, recording, locations },
         startRecording,
          stopRecording,
           changeName
-         } = useContext(LocationContext)
+         } = useContext(LocationContext);
 
+         const [saveTrack] = useSaveTrack();
     return <>
         <Spacer>
             <Input value={name} onChangeText={changeName} placeholder="Enter name"/>
@@ -21,7 +23,7 @@ const TrackForm = () => {
             }
             </Spacer>
             <Spacer>
-            {!recording && locations.length ? <Button title="Save Recording" /> : null}
+            {!recording && locations.length ? <Button onPress={saveTrack} title="Save Recording" /> : null}
         </Spacer>
     </>
 }
