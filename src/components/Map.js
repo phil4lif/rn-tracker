01d11bc69dbcd3fcdd9b8817389203482f5b7bd4
phil cowan <philipjcowan1@gmail.com>
2020-06-28 15:41:36 -0700

    adds back in polyline to draw a track

--- src/components/Map.js
+++ src/components/Map.js
@@ -4,8 +4,8 @@ import MapView, { Polyline, Circle } from 'react-native-maps';
 import { Context as LocationContext } from '../context/LocationContext';
 
 const Map = () => {
-    const { state: { currentLocation } } = useContext(LocationContext);
-    // console.log(state)
+    const { state: { currentLocation, locations } 
+    } = useContext(LocationContext);
     if (!currentLocation) {
         return <ActivityIndicator size="large" style={{ marginTop: 200 }} />;
     }
@@ -28,7 +28,7 @@ const Map = () => {
                 strokeColor="rgba(158, 158, 255, 1.0)"
                 fillColor="rgba(158, 158, 255, 0.3)"
                 />
-            {/* <Polyline coordinates={points} /> */}
+            <Polyline coordinates={locations.map(loc => loc.coords)} />
         </MapView>
     )
 }
